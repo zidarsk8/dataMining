e5355fd5306a3518f318be2934b224c49961b83e
Miha Zidar <zidarsk8@gmail.com>
2012-03-01 18:54:44 +0100

    added gain calculation. missing loop for all atributes and umber of random shufles

--- naloga02/src/naloga2.py
+++ naloga02/src/naloga2.py
@@ -4,11 +4,12 @@ import numpy
 import jrs
 import collections
 import itertools
+import pickle
 from sets import Set
 
 
+print "\n\ngenerating label counters"
 tl = open("minidata/trainingLabels.csv")
-
 labels = [i.strip().split(",") for i in tl.readlines()]
 labelCounter=collections.Counter(itertools.chain(*labels))
 
@@ -16,11 +17,41 @@ a = collections.defaultdict(list)
 [a[j].append(i) for i,j in labelCounter.most_common()]
 
 
+print "\nloading mld"
 mld=jrs.Data(discretized=True)
 
-for count,labels in a.items():
-	print count,labels
-	for label in labels:
-		print label
+data = mld.get_single_class_data("c40")
+	
 
-	#todo .. ALL THE THINGS
+
+	prvotnGain = Orange.feature.scoring.InfoGain(data.domain.features[0],data)
+	
+	a = [x.get_class().value for x in data]
+	a_original = list(a)
+	random.shuffle(a)
+	
+	for i,ex in enumerate(data):
+		ex.set_class(a[i])
+	
+	randomGain =  Orange.feature.scoring.InfoGain(data.domain.features[0],data)
+
+	for i,ex in enumerate(data):
+		ex.set_class(a_original[i])
+
+#mldRaw = pickle.load(open("minidata/trainingDataOD.pickled"))
+
+#print mld.domain.features
+#print mld.domain.class_var
+
+#for i in mld.domain.class_vars:
+#	print i
+
+#print mld[0][mld.domain.features[0]]
+
+#for count,labels in a.items():
+#	print count,labels
+#	mld.get_single_class_data("c40")
+#	for label in labels:
+#		print label
+#
+#	#todo .. ALL THE THINGS
