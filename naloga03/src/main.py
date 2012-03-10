4d5e26d3dd46452ac26a171b9058728014b51270
Miha Zidar <zidarsk8@gmail.com>
2012-03-10 23:15:56 +0100

    attr count does not matter for number of classes

--- naloga03/src/main.py
+++ naloga03/src/main.py
@@ -6,25 +6,16 @@ from itertools import chain as Chain
 
 d,ds = data.getDataArray()
 l = data.getLabelsArray()
+lc = Counter(Chain(*l))
 stPrimerov = len(l)
-stRazredov = max(map(max,l))+1
-stAtributov = max(map(max,d))+1
 
-print stRazredov
-print stPrimerov
-print stAtributov
-
+#print stPrimerov
 #print d
 #print ds
+#print lc
 
-lc = Counter(Chain(*l))
-print lc
-
-
-#stats = [[[0,ds[i][0],0,ds[i][1],0] for j in ds] for i in xrange(stRazredov)]
 
 stats = {}
-
 for p in xrange(stPrimerov):
 	for attr,val in d[p].items():
 		for clas in l[p]:
@@ -35,22 +26,30 @@ for p in xrange(stPrimerov):
 			stats[clas][attr][0] += val
 			stats[clas][attr][1] += 1
 
-
-#for i,j in enumerate(stats):
-#	print j
-#	for a in stats[j]:
-#		print "     ",a,stats[j][a],ds[a]
+# ta kos kode pokaze da stevilo razredov ni odvisno od
+# stevila nenicelnih atributov, saj je to povprecje enako
+# pri vseh stevilih razredov za posamezni primer.
+#lens = {}
+#for p in xrange(stPrimerov):
+#	if not lens.has_key(len(l[p])):
+#		lens[len(l[p])] = [0,0]
+#	lens[len(l[p])][0] += len(d[p])
+#	lens[len(l[p])][1] += 1
 #
+#for i,j in lens.items():
+#	print i,j, j[0]/j[1]
 
-at = 20
-cou = 0
-for i,a in enumerate(stats[at]):
-	if ds[a][1] > 1 and stats[at][a][1] > ds[a][1]*0.8:
-		cou += 1
-		print i,a,"  ",stats[at][a],"   ",ds[a]
 
-print lc[at],cou
 
+#at = 20
+#cou = 0
+#for i,a in enumerate(stats[at]):
+#	if ds[a][1] > 1 and stats[at][a][1] > ds[a][1]*0.8:
+#		cou += 1
+#		print i,a,"  ",stats[at][a],"   ",ds[a]
+#
+#print lc[at],cou
+#
 #print ds[9536]
 #
 #for i in xrange(stPrimerov):
@@ -59,9 +58,6 @@ print lc[at],cou
 #			stats[r][a][0] += d[i][a]
 #			stats[r][a][2] += d[i][a]
 #
-
-
-
 #for clas in xrange(stRazredov):
 #	for attr in xrange(stAtributov):
 #		if attr%10 == 0:
