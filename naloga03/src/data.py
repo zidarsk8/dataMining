e1daf4932620668d77b361d4aaeef32ea2d5aa8d
Miha Zidar <zidarsk8@gmail.com>
2012-03-11 15:37:23 +0100

    working k fold testin with simple line check

--- naloga03/src/data.py
+++ naloga03/src/data.py
@@ -14,23 +14,9 @@ def getDataArray(clean=False):
 		f = open("minidata/trainingData.csv")
 		#f = open("minidata/trainingD.csv")
 		a = [[int(y) for y in x.strip().split("\t")] for x in f.readlines()]
-		arr = []
-		for ii,i in enumerate(a):
-			arr.append({})
-			for c,j in enumerate(i):
-				if j != 0:
-					arr[ii][c] = j
-		sta = {}
-		for i in xrange(len(a[0])):
-			for j in xrange(len(a)):
-				if a[j][i] != 0:
-					if not sta.has_key(i):
-						sta[i]=[0,0]
-					sta[i][0] += a[j][i]
-					sta[i][1] += 1
-		pickle.dump((arr,sta),file(pickleFile,"w"),-1)
+		#pickle.dump(a,file(pickleFile,"w"),-1)
 		print "Reading complete: %.3fs" % (time()-t)
-	return (arr,sta)
+	return a
 
 def getLabelsArray(clean=False):
 	pickleFile = 'minidata/labelsArray.pickled'
@@ -39,7 +25,6 @@ def getLabelsArray(clean=False):
 		return pickle.load(open(pickleFile))
 	else:
 		t = time()
-		print "Reading trainingLabels.csv"
 		print "Reading trainingLabels.csv"
 		tl = open("minidata/trainingLabels.csv")
 		#tl = open("minidata/trainingL.csv")
--- naloga03/src/main.py
+++ naloga03/src/main.py
@@ -1,14 +1,33 @@
 #!/usr/bin/python2
 import data
 import sys
+from math import log
+from sets import Set
 from collections import Counter
 from itertools import chain as Chain
 
-def getLearningSet(d,l):
+def getDataDict(a):
+	arr = []
+	for ii,i in enumerate(a):
+		arr.append({})
+		for c,j in enumerate(i):
+			if j != 0:
+				arr[ii][c] = j
+	sta = {}
+	for i in xrange(len(a[0])):
+		for j in xrange(len(a)):
+			if a[j][i] != 0:
+				if not sta.has_key(i):
+					sta[i]=[0,0]
+				sta[i][0] += a[j][i]
+				sta[i][1] += 1
+	return (arr,sta)
+
+def getDataStats(d,l):
 	stats = {}
-	for p in xrange(len(d)):
-		for attr,val in d[p].items():
-			for clas in l[p]:
+	for p in xrange(len(d)): #za vsak primer
+		for attr,val in d[p].items(): #gremo po atributih
+			for clas in l[p]: # in vseh razredih
 				if not stats.has_key(clas):
 					stats[clas] = {}
 				if not stats[clas].has_key(attr):
@@ -24,85 +43,116 @@ def getLinePrediction(line,stats,ds,ls):
 		for ii, jj in j.items():
 			print jj
 
-def getPredictions(testData,stats,ds,ls):
+def getPredictionsEachAttr(trainD,trainL,testD):
 	result = []
+	d, ds = getDataDict(trainD)
+	l = list(trainL)
+	ls = Counter(Chain(*l))
+	stats = getDataStats(d,l)
+	for i in testD:
+		curRes = {}
+		for label, labelcount in ls.items():
+			curRes[label] = 0 # zacetna verjenost za razred i
+			for attr in testD[i]:
+				if testD[i][attr] != 0 and stats[label].has_key(attr):
+					curRes[label] += testD[i][attr]
+		result.append(curRes)
+	return result
+
+def dist(a,b):
+	l = min(len(a),len(b))
+	d = 0
+	maxD = 0
+	for i in xrange(l):
+		maxD += log(1+max(a[i],b[i]))
+		if min(a[i],b[i]) == 0:
+			d -= log(1+max(a[i],b[i]))
+		else:
+			d += log(1+min(a[i],b[i]))
+	return d #float(d)/maxD
+
+def distDict(a,b):
+	l = min(len(a),len(b))
+	k = Set(a.keys()+b.keys())
+	d = 0
+	for i in k:
+		if a.has_key(i) and b.has_key(i):
+			d += (1+min(a[i],b[i]))
+		#elif a.has_key(i):
+		#	d -= (1+a[i])
+		#elif b.has_key(i):
+		#	d -= (1+b[i])
+	return d #float(d)/maxD
+
+
+
+def getPredictionsRows(trainD,trainL,testD):
+	result = []
+	abc = len(testD)
+	train, trainStats = getDataDict(trainD)
+	test, testStats = getDataDict(testD)
+	for ab,primer in enumerate(test):
+		sys.stdout.flush()
+		sys.stdout.write("\r calculating classes %5d/%d " %(ab+1,abc))
+		dists = {}
+		for i,j in enumerate(train):
+			dists[i] = distDict(j,primer)
+		def sk(x): return x[1]
+		dd = sorted(dists.iteritems(), key=sk, reverse=True)
+		labs = []
+		avgLabs = 0
+		topK = 10
+		topKi = 4
+		for i in range(topKi):
+			labs += trainL[dd[i][0]]
+			avgLabs += len(trainL[dd[i][0]])
+		for i in range(topK):
+			labs += trainL[dd[i][0]]
+			avgLabs += len(trainL[dd[i][0]])
+		num = int((avgLabs/(topK+topKi)))
+		result.append([x[0] for x in Counter.most_common(Counter(labs),num)])
+	print
 	return result
 
 
 
-od,ds = data.getDataArray()
-ol = data.getLabelsArray()
-ls = Counter(Chain(*ol))
-stPrimerov = len(ol)
+labels = data.getLabelsArray(True)
+rawData = data.getDataArray(True)
+stPrimerov = len(labels)
+
+def precision(t,p):
+	return float(len(Set(t).intersection(Set(p)))) / len(p)
+
+def recall(t,p):
+	return float(len(Set(t).intersection(Set(p)))) / len(t)
+
+def fscore(t,p):
+	per = precision(t,p)
+	rec = recall(t,p)
+	return 2.0 * (per*rec) / (per+rec) if per+rec > 0 else 0
+
+def avgFscore(t,p):
+	l = len(t)
+	sum = 0
+	for i in xrange(l):
+		sum += fscore(t[i],p[i])
+	return sum/l
+
+
 
 k = 10;
-results = []
 print "starting %d fold cross validation" % k
 for i in xrange(k):
-	sys.stdout.write("\r%2s/%2d done" % (i+1,k))
+	#sys.stdout.write("\r%2s/%2d done" % (i+1,k))
 	f = stPrimerov/k * i
 	t = stPrimerov/k * (i+1)
-	# razdelimo podatke na 2 mnozici
-	d = od[:f]+od[t:]
-	testD = od[f:t]
-	l = ol[:f]+ol[t:]
-	testL = ol[f:t]
-	stats = getLearningSet(d,l)
-	results = getPredictions(testD,stats,ds,ls)
-print ""
-
-print results
+	trainD = rawData[:f]+rawData[t:]
+	trainL = labels[:f]+labels[t:]
+	testD = rawData[f:t]
+	testL = labels[f:t]
 	
-
-#print stPrimerov
-#print d
-#print ds
-#print lc
+	predictions = getPredictionsRows(trainD,trainL,testD)
+	
+	print "%2d fscore : %.6f" % (i, avgFscore(testL,predictions))
 
 
-# ta kos kode pokaze da stevilo razredov ni odvisno od
-# stevila nenicelnih atributov, saj je to povprecje enako
-# pri vseh stevilih razredov za posamezni primer.
-#lens = {}
-#for p in xrange(stPrimerov):
-#	if not lens.has_key(len(l[p])):
-#		lens[len(l[p])] = [0,0]
-#	lens[len(l[p])][0] += len(d[p])
-#	lens[len(l[p])][1] += 1
-#
-#for i,j in lens.items():
-#	print i,j, j[0]/j[1]
-
-
-
-#at = 20
-#cou = 0
-#for i,a in enumerate(stats[at]):
-#	if ds[a][1] > 1 and stats[at][a][1] > ds[a][1]*0.8:
-#		cou += 1
-#		print i,a,"  ",stats[at][a],"   ",ds[a]
-#
-#print lc[at],cou
-#
-#print ds[9536]
-#
-#for i in xrange(stPrimerov):
-#	for a in xrange(stAtributov):
-#		for r in l[i]:
-#			stats[r][a][0] += d[i][a]
-#			stats[r][a][2] += d[i][a]
-#
-#for clas in xrange(stRazredov):
-#	for attr in xrange(stAtributov):
-#		if attr%10 == 0:
-#			sys.stdout.flush()
-#			sys.stdout.write("\rGenerating statistics: %3.1f%%" % \
-#				(100.0*(clas*stAtributov+attr)/(stRazredov*stAtributov)))
-#		for i in xrange(stPrimerov):
-#			if l[clas][i]:
-#				stats[clas][attr][0] += d[i][attr]
-#				stats[clas][attr][2] += d[i][attr]==0
-#			else:
-#				stats[clas][attr][1] += d[i][attr]
-#				stats[clas][attr][3] += d[i][attr]==0
-#
