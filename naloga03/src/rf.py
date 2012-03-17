ffe3d31feb9f675a54c62e948424705faa7c7bd9
Miha Zidar <zidarsk8@gmail.com>
2012-03-17 02:37:31 +0100

    just made random forest file a bit nicer

--- naloga03/src/rf.py
+++ naloga03/src/rf.py
@@ -5,35 +5,37 @@ import Orange
 import jrs
 reload(jrs)
 
-##change empty features to 10 for real results and 40+ for testing
-#raw = jrs.RawData()
-#raw.remove_empty_features(10)
-#raw.convert_to_orange()
-#exit()
+#def prepairCsv(filter=10):
+#	#change empty features to 10 for real results and 40+ for testing
+#	raw = jrs.RawData()
+#	raw.remove_empty_features(filter)
+#	raw.convert_to_orange()
+#
+#prepairCsv(50)
 
-nof = 5
+numOfTrees = 5
 
 mld = jrs.Data()
 
 trainSet = mld.ml_data
 testSet = mld.test_data
 
-forest = Orange.ensemble.forest.RandomForestLearner(trees = nof)
+forest = Orange.ensemble.forest.RandomForestLearner(trees = numOfTrees)
 
 tl = len(testSet)
 cl = len(mld.classes)
 
 results = [{} for x in xrange(tl)]
+print "starting random forest"
 for ci, clas in enumerate(mld.classes):
 	model = forest(mld.get_single_class_data(clas))
 	for index, row in enumerate(testSet):
-		if index % 100 == 0:
-			sys.stdout.write("\rcalculating: %3d%%" % (((ci*tl)+index)*100/(tl*cl)))
-			sys.stdout.flush()
+		#if index % 100 == 0:
+		sys.stdout.write("\rcalculating: %3d%%" % (((ci*tl)+index)*100/(tl*cl)))
+		sys.stdout.flush()
 		res = model(row,Orange.classification.Classifier.GetProbabilities)["T"]
 		if res != 0:
 			results[index][clas] = res
-		
 print
 
 sortedRes = [sorted(x.iteritems(), key=lambda x: x[1], reverse=True) for x in results]
@@ -41,12 +43,6 @@ sortedRes = [sorted(x.iteritems(), key=lambda x: x[1], reverse=True) for x in re
 for i in sortedRes[1]:
 	print i 
 
-cPickle.dump(sortedRes,open("minidata/rf-sotedRes-%d-trees-%d.pickle" % (nof,time()),"w"))
-
-
-
-
-
-
+cPickle.dump(sortedRes,open("minidata/rf-sotedRes-%d-trees-%d.pickle" % (numOfTrees,time()),"w"))
 
 
