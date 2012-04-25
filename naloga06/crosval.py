699c33ea1ab2ce7abdf10d9634cd715d653f5f9b
Miha Zidar <zidarsk8@gmail.com>
2012-04-25 12:26:32 +0200

    crosval not working

--- naloga06/crosval.py
+++ naloga06/crosval.py
@@ -4,22 +4,36 @@ import sys
 import Orange
 import random
 
+def getProb(lrn,trainD,testD)
+    cl = lrn(trainD)
+    return [cl(t, Orange.classification.Classifier.GetProbabilities)[True] for t in testD]
+
 def randomForest(trainD,testD,trees=50):
     rf = Orange.ensemble.forest.RandomForestLearner(trees=trees, name="forest")
-    cl = rf(trainD)
-    return [cl(t, Orange.classification.Classifier.GetProbabilities)[True] for t in testD]
+    return getProb(rf, trainD, testD)
 
 def svm(trainD,testD):
     s = Orange.classification.svm.LinearSVMLearner(name="linearSVM")
-    cl = s(trainD)
-    return [cl(t, Orange.classification.Classifier.GetProbabilities)[True] for t in testD]
+    return getProb(rf, trainD, testD)
+
+def knn(trainD,testD):
+    s = Orange.classification.knn.kNNLearner(name="knn")
+    return getProb(rf, trainD, testD)
+
+#def logLoss2(yTrue,yPred):
+#    if len(yTrue) != len(yPred) : return -1
+#    N = len(yTrue)
+#    yTrue = np.array(yTrue)
+#    yPred = np.array(yPred)
+#    return -1.0/N * sum( yTrue*np.log(yPred) + (1-yTrue)*np.log(1-yPred) )
 
 def logLoss(yTrue,yPred):
     if len(yTrue) != len(yPred) : return -1
     N = len(yTrue)
     yTrue = np.array(yTrue)
-    yPred = np.array(yPred)
-    return -1.0/N * sum( yTrue*np.log(yPred) + (1-yTrue)*np.log(1-yPred) )
+    yTPred = np.array([x for i,x in enumerate(yPred) if yTrue[i]==1])
+    yFPred = np.array([x for i,x in enumerate(yPred) if yTrue[i]==0])
+    return -1.0/N *( sum(np.log(yTPred)) + sum(np.log(1-yFPred)) )
 
 
 print "loading data"
@@ -42,14 +56,14 @@ for fold in range(folds):
     sys.stdout.flush()
     trainD = data.select(cv_ind,fold,negate=1)
     testD = data.select(cv_ind,fold)
-    #yPred += randomForest(trainD, testD, 100)
-    rr = randomForest(trainD, testD, 10)
+    rr = randomForest(trainD, testD, 50)
+    #rr = svm(trainD, testD)
     ind = [i for i,j in enumerate(cv_ind) if j == fold]
     for i,r in enumerate(rr):
         yPred[ind[i]] = r
 
-yPred = [x if x>0 else 0.00001 for x in yPred]
-yPred = [x if x<1 else 0.99999 for x in yPred]
+yPred = [x if x>0 else 0.0001 for x in yPred]
+yPred = [x if x<1 else 0.9999 for x in yPred]
 #yPred = yPred*0.98+0.01
 
 _,yTrue,_ = data.to_numpy()
