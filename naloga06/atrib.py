315ac7816cb476b56a33b4019f99ee89e2647c53
Miha Zidar <zidarsk8@gmail.com>
2012-04-26 11:03:27 +0200

    neki

--- naloga06/atrib.py
+++ naloga06/atrib.py
@@ -4,9 +4,14 @@ import cPickle
 
 data = Orange.data.Table("data/train.tab")
 
-reliefScore = {}
-for attr in data.domain.attributes:
-    reliefScore[attr] = Orange.feature.scoring.Relief(attr, data)
-    print attr,reliefScore[attr]
+#reliefScore = {}
+#for attr in data.domain.attributes:
+#    reliefScore[attr] = Orange.feature.scoring.Relief(attr, data)
+#    print attr,reliefScore[attr]
+#cPickle.dump(reliefScore,open("reliefScore.pkl","w"))
+
+
+for i in range(len(data)):
+    print data[i][data.domain.attributes[0]]
     
-cPickle.dump(reliefScore,open("reliefScore.pkl","w"))
+    
--- naloga06/stats.py
+++ naloga06/stats.py
@@ -17,25 +17,38 @@ m,n = X.shape
 print "stevilo nul v celotni matriki %d/%d oziroma %.1f%%" %\
 		(sum(sum(X==0)),(n*m) , sum(sum(X==0))*100.0/(m*n))
 
+
+
 nenicelnih = sum(X != 0)
-plot.hist(nenicelnih,bins=50)
-plot.xlabel("st. nenicelnih vrednosti")
-plot.ylabel("st. atributov")
-plot.show()
-
+#plot.hist(nenicelnih,bins=50)
+#plot.xlabel("st. nenicelnih vrednosti")
+#plot.ylabel("st. atributov")
+#plot.show()
+#
 razlicnih = np.asarray([np.unique(X[:,i]).size for i in xrange(n)])
-plot.hist(razlicnih,bins=50,log=True)
-plot.xlabel("stevilo razlicnih vrednosti")
+#plot.hist(razlicnih,bins=50,log=True)
+#plot.xlabel("stevilo razlicnih vrednosti")
+#plot.ylabel("st. atributov")
+#plot.show()
+#
+#
+#razlicnihProcent = (razlicnih-1+(nenicelnih==m))*100.0/nenicelnih
+#plot.hist(razlicnihProcent,bins=50,log=True)
+#plot.xlabel("stevilo razlicnih v odvisnosti od stevila nenicelnih")
+#plot.ylabel("st. atributov")
+#plot.show()
+#
+
+maxStevilo = [x.max() for x in X.T]
+plot.hist(maxStevilo,bins=50)
+plot.xlabel("maksimalno stevilo v stolpcu")
 plot.ylabel("st. atributov")
 plot.show()
 
+means = [a.mean() for a in X.T]
 
-razlicnihProcent = (razlicnih-1+(nenicelnih==m))*100.0/nenicelnih
-plot.hist(razlicnihProcent,bins=50,log=True)
-plot.xlabel("stevilo razlicnih v odvisnosti od stevila nenicelnih")
-plot.ylabel("st. atributov")
-plot.show()
-
-
+for i in xrange(n):
+	print "%4d : razlicnih: %4d      nenicelnih: %4d           min: %5.3f          max: %5.3f        avg: %5.3f        <0.5 : %4d      <0.5 : %4d " % \
+			(i, razlicnih[i], nenicelnih[i], X.T[i].min(),X.T[i].max(),means[i],(X.T[i]<0.5).sum(),(X.T[i]>=0.5).sum())
 
 
